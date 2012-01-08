0d06bbddd816314711af5c930eb83141565bdb63
David Ross Drake <yurodivuie@gmail.com>
2012-01-08 15:01:56 -0800

    initial commit

---
+++ src/main/java/com/cegames/test/MethodLogger.java
@@ -0,0 +1,83 @@
+package com.cegames.test;
+
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class MethodLogger extends TestWatcher
+{
+    public static String DEFAULT_FORMAT = "Starting test [{}]";
+    
+    private final String messageFormat;
+    private Logger log = null;
+
+
+    /**
+     * Default constructor.  Will use the logger for the class under test,
+     * and will display messages using the DEFAULT_FORMAT.  Logger is
+     * obtained when the test method starts.
+     */
+    public MethodLogger() {
+        this.messageFormat = DEFAULT_FORMAT;
+    }
+
+    /**
+     * Constructor expects an slf4j Logger.  DEFAULT_FORMAT used for
+     * messages.
+     * 
+     * @param log   slf4j logger
+     */
+    MethodLogger(Logger log) {
+        this.messageFormat = DEFAULT_FORMAT;
+        this.log = log;
+    }
+
+    /**
+     * Constructor expects a logger and a format for how the message should
+     * be displayed.
+     *
+     * @param log   slf4j logger
+     * @param messageFormat Format for the message that will be displayed.
+     */
+    MethodLogger(Logger log, String messageFormat) {
+        this.log = log;
+        this.messageFormat = messageFormat;
+    }
+
+    /**
+     * Constructor expects a format for how the message should
+     * be displayed.  The logger will be obtained when the method is run,
+     * as per the no-arg constructor.
+     *
+     * @param messageFormat Format for the message that will be displayed.
+     */
+    MethodLogger(String messageFormat) {
+        this.messageFormat = messageFormat;
+    }
+
+    /**
+     * Whenever a test starts, this method logs the name of the test.
+     * @param description
+     */
+    @Override
+    protected void starting(Description description) {
+        prepareLogger(description);
+        log.info(messageFormat, description.getMethodName());
+    }
+
+    /**
+     * If logger is not initialized, initialize it with the name of the test class.
+     * @param description   Description provided by TestWatcher, used to get test class.
+     */
+    private void prepareLogger(Description description) {
+        if (log == null) {
+            log = LoggerFactory.getLogger(description.getClassName());
+        }
+    }
+    
+}
---
+++ src/test/java/com/cegames/test/MethodLoggerTest.java
@@ -0,0 +1,112 @@
+package com.cegames.test;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.mockito.ArgumentCaptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Unit test for simple MethodLogger.
+ */
+public class MethodLoggerTest {
+
+    private static Logger log = LoggerFactory.getLogger(MethodLoggerTest.class);
+
+    /**
+     * This is just to demonstrate to a user how it works.
+     */
+    @Rule
+    public static MethodLogger methodLogger = new MethodLogger();
+
+    @Test
+    public void testEmptyConstructor() {
+        MethodLogger defaultMethodLogger = new MethodLogger();
+
+        Appender<ILoggingEvent> mockAppender = mock(Appender.class);
+        ArgumentCaptor<ILoggingEvent> argument = ArgumentCaptor.forClass(ILoggingEvent.class);
+
+        try {
+            ((ch.qos.logback.classic.Logger) log).addAppender(mockAppender);
+
+            Description description = mock(Description.class);
+
+            when(description.getClassName()).thenReturn(MethodLoggerTest.class.getName());
+            when(description.getMethodName()).thenReturn("mockMethod");
+
+            defaultMethodLogger.starting(description);
+
+            verify(mockAppender).doAppend(argument.capture());
+            assertThat(argument.getValue().getLoggerName(), equalToIgnoringCase(MethodLoggerTest.class.getName()));
+            assertThat(argument.getValue().getMessage(), equalToIgnoringCase(MethodLogger.DEFAULT_FORMAT));
+        } finally {
+            ((ch.qos.logback.classic.Logger) log).detachAppender(mockAppender);
+        }
+    }
+    
+    @Test
+    public void testLoggerOnlyConstructor() {
+        String methodName = "testMethod";
+        Logger mlog = mock(Logger.class);
+        Description description = mock(Description.class);
+        when(description.getMethodName()).thenReturn(methodName);
+        
+        MethodLogger testMethodLogger = new MethodLogger(mlog);
+        testMethodLogger.starting(description);
+        
+        verify(mlog).info(MethodLogger.DEFAULT_FORMAT, methodName);
+    }
+
+    @Test
+    public void testLoggerFormatConstructor() {
+        String methodName = "anotherTest";
+        String testFormat = "TEST - <{}>!!!!";
+        Logger mlog = mock(Logger.class);
+        Description description = mock(Description.class);
+        when(description.getMethodName()).thenReturn(methodName);
+
+        MethodLogger testMethodLogger = new MethodLogger(mlog, testFormat);
+        testMethodLogger.starting(description);
+
+        verify(mlog).info(testFormat, methodName);
+    }
+
+    @Test
+    public void testFormatOnlyConstructor() {
+        String testFormat = "Fancy test started ~@~ +|{}|+";
+        String methodName = "mockMethod";
+        
+        MethodLogger defaultMethodLogger = new MethodLogger(testFormat);
+
+        Appender<ILoggingEvent> mockAppender = mock(Appender.class);
+        ArgumentCaptor<ILoggingEvent> argument = ArgumentCaptor.forClass(ILoggingEvent.class);
+
+        try {
+            ((ch.qos.logback.classic.Logger) log).addAppender(mockAppender);
+
+            Description description = mock(Description.class);
+
+            when(description.getClassName()).thenReturn(MethodLoggerTest.class.getName());
+            when(description.getMethodName()).thenReturn(methodName);
+
+            defaultMethodLogger.starting(description);
+
+            verify(mockAppender).doAppend(argument.capture());
+
+            assertThat(argument.getValue().getLoggerName(), equalToIgnoringCase(MethodLoggerTest.class.getName()));
+            assertThat(argument.getValue().getMessage(), equalToIgnoringCase(testFormat));
+        } finally {
+            ((ch.qos.logback.classic.Logger) log).detachAppender(mockAppender);
+        }
+    }
+}
